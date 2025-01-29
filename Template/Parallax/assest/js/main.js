72edffbb439210f0fdfbb08123bb50b74a1576fc
Bui The Trong <trongbuithe06@gmail.com>
2025-01-29 22:24:55 +0700

    cmt
    header, banner (responsive)

---
+++ Template/Parallax/assest/js/main.js
@@ -0,0 +1,25 @@
+const navbar = document.querySelector(".header__navbar-icon")
+const dropdown = document.querySelector(".header__dropdow")
+console.log(navbar)
+
+navbar.addEventListener('click', function (e) {
+    e.stopPropagation(); // Ngăn chặn sự kiện click lan đến document
+    dropdown.classList.toggle('active'); // Thêm/xóa class 'active'
+    navbar.style.backgroundColor = '#000'
+    navbar.style.color = '#fff'
+    if (!dropdown.classList.contains('active')) {
+        navbar.style.backgroundColor = 'transparent'
+        navbar.style.color = '#555'
+    }
+
+})
+
+// Ẩn menu khi nhấn vào vùng bên ngoài
+document.addEventListener('click', (event) => {
+    if (dropdown.classList.contains('active') && !dropdown.contains(event.target)) {
+        dropdown.classList.remove('active');
+        navbar.style.backgroundColor = 'transparent'
+        navbar.style.color = '#555'
+    }
+});
+
