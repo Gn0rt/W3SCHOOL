6f71e110bb955aea4afc38130e33b8a56c3c08e3
Bui The Trong <trongbuithe06@gmail.com>
2025-02-03 09:21:11 +0700

    cmt
    done

--- Template/Parallax/assest/js/main.js
+++ Template/Parallax/assest/js/main.js
@@ -1,7 +1,7 @@
 const navbar = document.querySelector(".header__navbar-icon")
 const dropdown = document.querySelector(".header__dropdow")
 console.log(navbar)
-
+const backToTopBtn = document.querySelector(".backToTop")
 navbar.addEventListener('click', function (e) {
     e.stopPropagation(); // Ngăn chặn sự kiện click lan đến document
     dropdown.classList.toggle('active'); // Thêm/xóa class 'active'
@@ -21,5 +21,15 @@ document.addEventListener('click', (event) => {
         navbar.style.backgroundColor = 'transparent'
         navbar.style.color = '#555'
     }
+});
+
+
+// Cuộn về đầu trang khi nhấn vào nút
+backToTopBtn.addEventListener('click', (event) => {
+    event.preventDefault(); // Ngăn chặn hành vi mặc định của liên kết
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth' // Cuộn mượt mà
+    });
 });
 
